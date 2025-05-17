ee380bb928d9c2e69d21042e84a4afbad553e74e
liljdot <jehuoki@gmail.com>
2025-05-17 20:47:35 +0100

    send response with POST request body

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
 
     console.log("received a post request", req.body.message.text)
+    res.send(req.body)
 })
 
 app.listen(3000, () => {
