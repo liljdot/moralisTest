e4e4aae218068ee801cc64fa461b92123e530068
liljdot <jehuoki@gmail.com>
2025-05-17 20:36:46 +0100

    log specific message text from POST request body

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
 
-    console.log("received a post request", req.body)
+    console.log("received a post request", req.body.message.text)
 })
 
 app.listen(3000, () => {
